e24f16779869ccdac9e77b54814ff83a2ac78b08
akcgjc007 <akcgjc007@gmail.com>
2021-02-01 18:58:39 +0530

    added instructions in README.md

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
+# Kokken
+
 ## How to run?
  - `npm i`
  - `npm run compile`
  - `npm run test`
 
-
-`artifacts`: compiled scripts
-`migrations`: migrate scripts
-`test`: testing scripts
-
+## Directories
+### Permanent:
+ - `contracts`: smart contract scripts
+ - `migrations`: migration scripts
+ - `test`: testing scripts
+### Temporary scripts
+ - `artifacts`: compiled scripts, needed for integrating with front-end
+ 
+    We need the `interface` files of the compiled contracts.
+    After smart contract development, we can write a script which will dump `interface` code into a separate file. This separate file will be downloaded by the user to interact with contracts.
+ 
+## VS Code Extensions
+ - Solidity by Juan Blanco: error messages in the editor
+ - Prettier - Code Formatter: formatting js files
--- test/1_Market_tests.js
+++ test/1_Market_tests.js
@@ -1,79 +1,73 @@
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const {
+  isCallTrace,
+} = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 let market;
 let marketFactory;
 
 let marketAddress;
 let accounts;
+
 let admin;
 let owner;
-let addr1;
+let user;
 
 let provider = ethers.getDefaultProvider();
 
+const toTimestamp = (strDate) => Date.parse(strDate) / 1000;
 
-const toTimestamp = (strDate)=>Date.parse(strDate)/1000;
+beforeEach(async () => {
+  accounts = await ethers.getSigners();
+  admin = accounts[0];
+  owner = accounts[1];
+  user = accounts[2];
 
+  let marketObject = await ethers.getContractFactory("EIP1167_Market");
+  market = await marketObject.deploy();
 
+  marketAddress = await market.address;
 
+  let factoryObject = await ethers.getContractFactory("EIP1167_MarketFactory");
+  marketFactory = await factoryObject.connect(admin).deploy(marketAddress);
 
-beforeEach(async ()=>{
-    accounts = await ethers.getSigners();
-    admin = accounts[0];
-    owner = accounts[1];
-    addr1 = accounts[2];
-    
-
-    let marketObject = await ethers.getContractFactory('EIP1167_Market');
-    market = await marketObject.deploy();
-
-    marketAddress = await market.address;
-
-    let factoryObject = await ethers.getContractFactory('EIP1167_MarketFactory');
-    marketFactory = await factoryObject.connect(admin).deploy(marketAddress);
-
-
-    await marketFactory
-        .connect(owner)
-        .createMarket(
-            "Who do we think we are?", 
-            2, 
-            ["Humans", "Animals"], 
-            toTimestamp('01/01/2029 00:00:00')
-        );    
+  await marketFactory
+    .connect(owner)
+    .createMarket(
+      "Who do we think we are?",
+      2,
+      ["Humans", "Animals"],
+      toTimestamp("01/01/2029 00:00:00")
+    );
 });
 
-describe("Market contract", ()=>{
-    
-    // it("has the same as Factory contract instance.", async()=>{
-    //     //The big number at the end of the createMarket function is Unix Epoch Time of some moment in the future.
-    //     await marketFactory
-    //     .connect(owner)
-    //     .createMarket(
-    //         "Who do we think we are?", 
-    //         2, 
-    //         ["Humans", "Animals"], 
-    //         toTimestamp('01/01/2029 00:00:00')
-    //     );    
+describe("Market contract", () => {
+  // it("has the same as Factory contract instance.", async()=>{
+  //     //The big number at the end of the createMarket function is Unix Epoch Time of some moment in the future.
+  //     await marketFactory
+  //     .connect(owner)
+  //     .createMarket(
+  //         "Who do we think we are?",
+  //         2,
+  //         ["Humans", "Animals"],
+  //         toTimestamp('01/01/2029 00:00:00')
+  //     );
 
-    //     assert(marketFactory.markets(0).bytecode == market.bytecode);
-    // });
+  //     assert(marketFactory.markets(0).bytecode == market.bytecode);
+  // });
 
-    it("is setting the owner correctly.", async()=>{
-        
-        let firstDeployedQuestionAddress = await marketFactory.markets(0);
-        let abi = (await ethers.getContractFactory("Market")).interface;
+  it("is setting the owner correctly.", async () => {
+    let firstDeployedQuestionAddress = await marketFactory.markets(0);
+    let abi = (await ethers.getContractFactory("Market")).interface;
 
-        let question = new ethers.Contract(
-            firstDeployedQuestionAddress,
-            abi,
-            provider
-        );
+    let question = new ethers.Contract(
+      firstDeployedQuestionAddress,
+      abi,
+      provider
+    );
 
-        let _owner = await question.connect(owner).owner();
-        assert.strictEqual(_owner, owner.address);
-
-    });
+    let _owner = await question.connect(owner).owner();
+    assert.strictEqual(_owner, owner.address);
+  });
 });
