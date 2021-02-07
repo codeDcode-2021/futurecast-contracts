65651f00a24613c65bc0f095aa3375e799b4ca4f
rashtrakoff <chinmay.vemuri@outlook.com>
2021-02-07 17:28:47 +0530

    Merge branch 'master' of https://github.com/codeDcode-2021/smart-contract-testing

--- contracts/Question/EIP1167_Question.sol
+++ contracts/Question/EIP1167_Question.sol
@@ -5,36 +5,97 @@ pragma abicoder v2;
 import "./console.sol";
 import { SafeMath } from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/math/SafeMath.sol";
 
-
-
-contract EIP1167_Question
+contract EIP1167_Question 
 {
     using SafeMath for uint256;
     
-    enum State {Betting, Declared, Reporting, Resolved}
+    enum State {BETTING, DECLARED, REPORTING, RESOLVED}
+    State private currState;
     
     address public owner;
     string public question;
     string[] public options;
     uint256 public endTime;  
     uint256[] public optionBalances;
-
+    uint256 public marketPool;
+    uint256 public validationPool;
+    uint256 constant private MARKET_MAKER_FEE_PER = 995; // 0.5% for now.
+    
+    /// @dev mapping(address=>mapping(optionId=>stake))
+    mapping(address => mapping(uint256=>uint256)) public stakeDetails; // Change the visibility to private after testing
+    mapping(address => bool) public hasVoted; // Change the visibility to private after testing
+    
+    modifier checkState(State _state)
+    {
+        require(currState == _state, "This function is not allowed in the current phase of the market");
+        _;
+    }
+    
+    modifier onlyOwner
+    {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+    
+    modifier validOption(uint256 _optionId)
+    {
+        /// @dev _optionId represents the index of the option.
+        require(_optionId >= 0 && _optionId < options.length);
+        _;
+    }
+    
     function init(address _owner, string calldata _question, string[] memory _options, uint256 _endTime) external
     {
         /***
          * @dev Function for creating a market
          */
-         
         owner = _owner;
         question = _question;
         options = _options;
         endTime = _endTime;
+        currState = State.BETTING;
         
         // Code for testing purpose
         // console.log("Address of this contract is %s", address(this));
         // console.log("Owner of this contract is %s", owner);
     }
     
+    function stake(uint256 _optionId) external payable checkState(currState) validOption(_optionId)
+    {
+        /***
+         * Test this function for rounding errors.
+         * Check if there is a better way to collect fees.
+         * Uncomment the lines in this function after importing validationFee code.
+         */
+        uint256 amount = msg.value;
+        uint256 marketMakerFee = amount.sub(MARKET_MAKER_FEE_PER*amount/1000);
+        //uint256 validationFee = amount.sub((VALIDATION_FEE.sub(MARKET_MAKER_FEE_PER))*amount/1000)
+        //uint256 stakeAmount = amount.sub(marketMakerFee.add(validationFee));
+        uint256 optionStakeAmount = stakeDetails[msg.sender][_optionId];
+        marketPool = marketPool.add(marketMakerFee);
+        //validationPool = validationPool.add(validationFee);
+        
+        //stakeDetails[msg.sender][_optionId] = optionStakeAmount.add(stakeAmount);
+        hasVoted[msg.sender] = true;
+    }
+    
+    
+    function changeStake(uint256 _fromOptionId, uint256 _toOptionId, uint256 _amount) external checkState(currState) validOption(_fromOptionId) validOption(_toOptionId)
+    {
+        /***
+         * This function allows the user to change the stake from one option to another option.
+         * Discuss whether stake change must be taxed by market maker. The code for this case has not been written.
+         */
+        require(hasVoted[msg.sender], "You haven't staked before !");
+        require(stakeDetails[msg.sender][_fromOptionId] >= _amount, "Stake change amount is higher than the staked amount !");
+        require(_fromOptionId != _toOptionId, "Options are the same !");
+        require(_amount > 0, "Insufficient stake change amount"); // Is this required ?
+    
+        uint256 _fromOptionStakedAmount = stakeDetails[msg.sender][_fromOptionId];
+        uint256 _toOptionStakedAmount = stakeDetails[msg.sender][_toOptionId];
+        stakeDetails[msg.sender][_fromOptionId] = _fromOptionStakedAmount.sub(_amount);
+        stakeDetails[msg.sender][_toOptionId] = _toOptionStakedAmount.add(_amount);
+    }
     
         
 }
