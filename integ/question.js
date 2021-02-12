cc71ed8e09f3292419d19b3365ebeb3b8fff5a42
akcgjc007 <akcgjc007@gmail.com>
2021-02-12 13:42:03 +0530

    Updated all integration files.

--- integ/question.js
+++ integ/question.js
@@ -1,8 +1,8 @@
 // Preconfigured instance of the contract
-import web3 from './web3';
-import compiledContract from './build/Election.json';
+import web3 from './web3-hardhat';
+import info from './info.json';
 
 export default async function questionInstance (deployedAddress){
-    return (await new web3.eth.Contract(compiledQuestion.abi, deployedAddress))
+    return (await new web3.eth.Contract(info.questionInterface, deployedAddress))
       .methods;
 }
